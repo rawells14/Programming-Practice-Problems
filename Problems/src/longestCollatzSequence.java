2c067d6558ebd7c493e474ab3848cd7ba48a3409
Ryan Wells <rawells14@gmail.com>
2014-10-09 12:11:27 -0500

    Changed Algorithm for CollatzSequence

--- Problems/src/longestCollatzSequence.java
+++ Problems/src/longestCollatzSequence.java
@@ -2,7 +2,35 @@
 public class longestCollatzSequence {
 
     public static void main(String[] args) {
-        int current = 0;
+        long largest = 0;
+        long largestnum = 0;
+        long time = System.currentTimeMillis();
+        System.currentTimeMillis();
+        for (int i = 1000000; i>=2; i--) {
+            if (sequence(i) > largest) {
+                largest = sequence(i);
+                largestnum =i;
+            }
+
+        }
+        System.out.println(largest);
+        System.out.println(largestnum);
+        System.out.println("Run Time: " + (System.currentTimeMillis() - time) + " milliseconds");
     }
 
+    public static long sequence(long n) {
+        long amt = 1;
+        while (true) {
+            if (n == 1) {
+                return amt;
+            }
+            if (n % 2 == 0) {
+                n /= 2;
+                amt++;
+            } else if (n % 2 != 0) {
+                n = (3 * n) + 1;
+                amt++;
+            }
+        }
+    }
 }
