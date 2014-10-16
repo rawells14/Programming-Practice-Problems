580b59326762e962e544a4580a2a89bd755a6b58
Ryan Wells <rawells14@gmail.com>
2014-10-16 10:54:35 -0500

    Amicable Numbers and Name Scores algorithm finished

--- Problems/src/amicableNumbers.java
+++ Problems/src/amicableNumbers.java
@@ -1,13 +1,33 @@
 
 public class amicableNumbers {
 
+    public static double Timer(double started) {
+        return System.currentTimeMillis() - started;
+    }
+
     public static void main(String[] args) {
-        System.out.println(d(220   ));
+        double time = System.currentTimeMillis();
+        int tot = 0;
+        System.out.println(d(8128));
+        for (int i = 30; i < 10000; i += 2) {
+            for (int j = 30; j < 10000; j += 2) {
+                if (i != j) {
+                    if (d(i) == j && d(j) == i) {
+                        System.out.println(i + "  " + j);
+                        i = j;
+                        tot += i + j;
+                    }
+                }
+            }
+
+        }
+        System.out.println(tot);
+        System.out.println("Run Time: " + Timer(time) / 1000 + " seconds");
     }
 
     public static int d(int n) {
         int sumOfDivisors = 0;
-        for (int i = n; i >= 1; i--) {
+        for (int i = 1; i < n; i++) {
             if (n % i == 0) {
                 sumOfDivisors += i;
             }
--- Problems/src/nameScores.java
+++ Problems/src/nameScores.java
@@ -1,20 +1,34 @@
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class nameScores {
 
+    public static void Timer(double started) {
+        System.out.print((System.currentTimeMillis() - started) / 1000+" seconds");
+    }
+
     public static void main(String[] args) throws FileNotFoundException {
+        double time = System.currentTimeMillis();
         Scanner asdf = new Scanner(new File("names.txt"));
         String data[] = asdf.nextLine().replaceAll("\"", "").split(",");
-        long tot = 0;
+        ArrayList<String> arr = new ArrayList();
         for (int i = 0; i < data.length; i++) {
-            tot+= calculateNameScore(data[i],i+1);
+            arr.add(data[i]);
+        }
+        Collections.sort(arr);
+        Timer(time);
+        System.out.println(" to initialize and sort data");
+        long tot = 0;
+        for (int i = 0; i < arr.size(); i++) {
+            tot += calculateNameScore(arr.get(i), i + 1);
         }
         System.out.println(tot);
-
-        
+        Timer(time);
+        System.out.println(" to calcualte name scores.");
     }
 
     public static int calculateNameScore(String name, int index) {
